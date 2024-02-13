9c9b9b344ead6176ddec6749448262568bdefff4
pKostantine <150958805+pKostantine@users.noreply.github.com>
2024-02-13 10:52:28 -0500

    Add files via upload

---
+++ Mythology in the Media/Asclepius Online Pharmacy.js
@@ -0,0 +1,21 @@
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', event => {
+//   button.textContent = `Click count: ${event.detail}`;
+// });
+
+// function myFunction() {
+//     location.replace("https://www.w3schools.com")
+//   }
+
+function unprescribedMedication() {
+    window.location.href = "Unprescribed Medication.html"
+}
+
+function prescribedMedication() {
+    window.location.href = "Prescribed Medication.html"
+}
+
+function feedback() {
+    window.location.href = "Feedback.html"
+}
---
+++ Mythology in the Media/Prescribed Medication.js
@@ -0,0 +1,11 @@
+function unprescribedMedication() {
+    window.location.href = "Unprescribed Medication.html"
+}
+
+function prescribedMedication() {
+    window.location.href = "Prescribed Medication.html"
+}
+
+function feedback() {
+    window.location.href = "Feedback.html"
+}
